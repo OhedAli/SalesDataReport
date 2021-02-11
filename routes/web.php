72e141e10496615aa40b025a264cdc04fe1bcb49
OhedAli <ohed.ali@codeclouds.in>
2021-02-11 18:10:59 +0530

    dashboard page fully dynamic

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SaleslogsController;
+use App\Http\Controllers\ProfileController;
 
 
 /*
@@ -25,16 +26,14 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/dashboard',[DashboardController::class, 'index','getRecord'])->name('dashboard');
 Route::get('/sales',[SaleslogsController::class, 'index'])->name('saleslogs');
 Route::get('/sales/{id}',[SaleslogsController::class, 'show'])->name('saleslogs-show');
-//Route::resource('/profile',[ProfileController::class, 'index'])->name('profile');
+Route::get('/profile',[ProfileController::class, 'index'])->name('profile');
 
 });
 
 
-
-
-Route::get('/profile-page', function(){
-return view('profile-page');
-})->middleware(['auth'])->name('view.profile');
+//Route::get('/profile-page', function(){
+//return view('profile-page');
+//})->middleware(['auth'])->name('view.profile');
 
 
 require __DIR__.'/auth.php';
